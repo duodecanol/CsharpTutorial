db1ddbff3c3b53c48c307ad193086b2085ad1047
HOME-2700 <metalhwal@gmail.com>
2021-11-06 18:23:28 +0900

    Add project files.

---
+++ AwaitTutorial/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AwaitTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // ExampleMethodAsync returns a Task<int>, which means that the method eventually produces
+            // an int result. However, ExampleMothodAsync returns the Task<int> value as soon as it reaches an await.
+
+            ResultsTextBox.Text += "\n"; // The TextBox object from MainWindow.
+
+            try
+            {
+                int lenght = await 
+            }
+        }
+    }
+}
